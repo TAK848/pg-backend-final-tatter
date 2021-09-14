e9bd3fad4617a5fbb58d4c10f8e31778de9d0d45
TAK848 <pairs_char0j@icloud.com>
2021-09-14 18:37:30 +0900

    Tart modelを一部変更

--- tart/models.py
+++ tart/models.py
@@ -4,16 +4,23 @@ from string import ascii_letters, digits
 from django.contrib.auth import get_user_model
 from django.db import models
 
+from .validate import linebreak_limit_tart
+
 User = get_user_model()
 
 
 def make_random_tart_id(n=15):
     dat = ascii_letters + digits + '-_'
-    print(ascii_letters)
     while True:
         made_id = ''.join([random.choice(dat) for i in range(n)])
         if not Tart.objects.filter(id=made_id).exists():
             return made_id
+
+
+class TartManager(models.Manager):
+    def create_tart(self, user, tart_text):
+        tart = self.create(user=user, text=tart_text)
+        return tart
 
 
 class Tart(models.Model):
@@ -32,7 +39,12 @@ class Tart(models.Model):
     text = models.TextField(
         'Tart内容',
         max_length=140,
+        validators=[
+            linebreak_limit_tart,
+        ]
     )
-    created_at = models.DateTimeField('作成日', auto_now_add=True)
-    updated_at = models.DateTimeField('更新日', auto_now=True)
+    created_at = models.DateTimeField('作成日時', auto_now_add=True)
+    updated_at = models.DateTimeField('更新日時', auto_now=True)
     was_edited = models.BooleanField('編集済', default=False)
+
+    objects = TartManager()
